04a79c48cbb46994310e93c9ada69496130b8f0d
khushi27-02 <92244668+khushi27-02@users.noreply.github.com>
2023-09-12 13:13:02 +0530

    Add files via upload

---
+++ Tic Tac Toe.py
@@ -0,0 +1,52 @@
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+    
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+    
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    
+    return False
+
+def is_board_full(board):
+    return all(cell != " " for row in board for cell in row)
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    players = ["X", "O"]
+    turn = 0
+
+    while True:
+        print_board(board)
+        player = players[turn % 2]
+        print(f"Player {player}'s turn:")
+        
+        row = int(input("Enter the row (0, 1, 2): "))
+        col = int(input("Enter the column (0, 1, 2): "))
+        
+        if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == " ":
+            board[row][col] = player
+            if check_winner(board, player):
+                print_board(board)
+                print(f"Player {player} wins!")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a draw!")
+                break
+            turn += 1
+        else:
+            print("Invalid move. Try again.")
+
+if __name__ == "__main__":
+    main()
